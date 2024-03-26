607143b862a15a44922875c826f7e0721c2fad1f
Jason Young <148194611+jasonyoung23@users.noreply.github.com>
2024-03-26 18:18:26 +0800

    Add files via upload

---
+++ debug_lab_1/P5/p5.cpp
@@ -0,0 +1,72 @@
+#include "stack.h"
+#include <iostream>
+
+using namespace std;
+
+Stack::Stack(int size){
+        this->size = size;
+        this->index = 0;
+        ptr = new int[size];
+}
+
+Stack::Stack(const Stack &s){
+        size = s.size;
+        index = s.index;
+        ptr = new int[size];
+        for (int i = 0; i < index; i++){
+                ptr[i] = s.ptr[i];
+        }
+}
+
+Stack::Stack(Stack &&s){
+        size = s.size;
+        this->index = 0;
+        ptr = s.ptr;
+        // delete [] a nullptr is safe
+        s.ptr = nullptr;
+}
+
+Stack::~Stack(){
+        delete [] ptr;
+}
+
+Stack &Stack::operator=(const Stack &s){
+        if (this == &s)
+                return *this;
+
+        delete [] ptr;
+        ptr = new int[s.size];
+        size = s.size;
+        index = s.index;
+        for(int i = 0; i <  index; i++) ptr[i] = s.ptr[i];
+
+}
+
+Stack &Stack::operator=(Stack &&s){
+        if(this == &s)
+
+        return *this;
+}
+
+void Stack::push(int val){
+        if (index < size)
+                ptr[index++] = val;
+}
+
+int Stack::pop(){
+        if (!index)
+                return -1;
+
+        return ptr[--index];
+}
+
+int Stack::top(){
+        if (!index)
+                return -1;
+
+        return ptr[index - 1];
+}
+
+bool Stack::empty(){
+        return !index;
+}
